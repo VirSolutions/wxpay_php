1380369b8f547093228a93e8b7ebd0a751887106
William <zhibin.xi@gmail.com>
2016-09-02 16:59:59 +0800

    Prod online version 1.0.0

--- PROD/www/php/vendor/dwj/index.php
+++ PROD/www/php/vendor/dwj/index.php
@@ -30,16 +30,16 @@ $notify = new NativePay();
  */
 $WxPayUnifiedOrder = new WxPayUnifiedOrder();
 $inputPid001 = &$WxPayUnifiedOrder;
-$inputPid001 ->SetBody("大玩家万达店_水果忍者");
-$inputPid001 ->SetAttach("Products001");
+$inputPid001 ->SetBody("大玩家金牛万达店_水果先锋");
+$inputPid001 ->SetAttach("大玩家金牛万达店");
 $inputPid001 ->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-$inputPid001 ->SetTotal_fee("1");
+$inputPid001 ->SetTotal_fee("1000");
 $inputPid001 ->SetTime_start(date("YmdHis"));
 $inputPid001 ->SetTime_expire(date("YmdHis", time() + 3600));
-$inputPid001 ->SetGoods_tag("水果忍者");
+$inputPid001 ->SetGoods_tag("水果先锋");
 $inputPid001 ->SetNotify_url("http://60.205.127.180/wxengine/logic/notify.php");
 $inputPid001 ->SetTrade_type("NATIVE");
-$inputPid001 ->SetProduct_id("123456781");
+$inputPid001 ->SetProduct_id("Fruit_DWJ_JNWD_S");
 $resultPid001 = $notify->GetPayUrl($inputPid001);
 $urlPid001 = $resultPid001 ["code_url"];
 
@@ -47,16 +47,16 @@ sleep(1);
 
 /**$inputPid002 = new WxPayUnifiedOrder(); */
 $inputPid002 = &$WxPayUnifiedOrder;
-$inputPid002 ->SetBody("大玩家万达店_火力全开");
-$inputPid002 ->SetAttach("Products002");
+$inputPid002 ->SetBody("大玩家金牛万达店_全面开火");
+$inputPid002 ->SetAttach("大玩家金牛万达店");
 $inputPid002 ->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-$inputPid002 ->SetTotal_fee("2");
+$inputPid002 ->SetTotal_fee("3000");
 $inputPid002 ->SetTime_start(date("YmdHis"));
 $inputPid002 ->SetTime_expire(date("YmdHis", time() + 3600));
-$inputPid002 ->SetGoods_tag("火力全开");
+$inputPid002 ->SetGoods_tag("全面开火");
 $inputPid002 ->SetNotify_url("http://60.205.127.180/wxengine/logic/notify.php");
 $inputPid002 ->SetTrade_type("NATIVE");
-$inputPid002 ->SetProduct_id("123456782");
+$inputPid002 ->SetProduct_id("Fire_DWJ_JNWD_S");
 $resultPid002 = $notify->GetPayUrl($inputPid002);
 $urlPid002 = $resultPid002 ["code_url"];
 
@@ -64,18 +64,35 @@ sleep(1);
 
 /**$inputPid003 = new WxPayUnifiedOrder();*/
 $inputPid003 = &$WxPayUnifiedOrder;
-$inputPid003 ->SetBody("大玩家万达店_最高危机");
-$inputPid003 ->SetAttach("Products003");
+$inputPid003 ->SetBody("大玩家金牛万达店_水果先锋_双人");
+$inputPid003 ->SetAttach("大玩家金牛万达店");
 $inputPid003 ->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-$inputPid003 ->SetTotal_fee("1");
+$inputPid003 ->SetTotal_fee("2000");
 $inputPid003 ->SetTime_start(date("YmdHis"));
 $inputPid003 ->SetTime_expire(date("YmdHis", time() + 3600));
-$inputPid003 ->SetGoods_tag("最高危机");
+$inputPid003 ->SetGoods_tag("水果先锋");
 $inputPid003 ->SetNotify_url("http://60.205.127.180/wxengine/logic/notify.php");
 $inputPid003 ->SetTrade_type("NATIVE");
-$inputPid003 ->SetProduct_id("123456783");
+$inputPid003 ->SetProduct_id("Fruit_DWJ_JNWD_D");
 $resultPid003 = $notify->GetPayUrl($inputPid003);
 $urlPid003 = $resultPid003 ["code_url"];
+
+sleep(1);
+
+/**$inputPid004 = new WxPayUnifiedOrder();*/
+$inputPid004 = &$WxPayUnifiedOrder;
+$inputPid004 ->SetBody("大玩家金牛万达店_全面开火_双人");
+$inputPid004 ->SetAttach("大玩家金牛万达店");
+$inputPid004 ->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+$inputPid004 ->SetTotal_fee("6000");
+$inputPid004 ->SetTime_start(date("YmdHis"));
+$inputPid004 ->SetTime_expire(date("YmdHis", time() + 3600));
+$inputPid004 ->SetGoods_tag("全面开火");
+$inputPid004 ->SetNotify_url("http://60.205.127.180/wxengine/logic/notify.php");
+$inputPid004 ->SetTrade_type("NATIVE");
+$inputPid004 ->SetProduct_id("Fruit_DWJ_JNWD_D");
+$resultPid004 = $notify->GetPayUrl($inputPid004);
+$urlPid004 = $resultPid004 ["code_url"];
 
 			
 if (empty($urlPid001)){
@@ -96,6 +113,11 @@ if (empty($urlPid003)){
 		echo "PID003 URL: " .$urlPid003;
 }
 
+if (empty($urlPid004)){
+	echo "PID004 not set";
+} else {
+		echo "PID004 URL: " .$urlPid004;
+}
 
 
 ?>
@@ -127,16 +149,16 @@ if (empty($urlPid003)){
 }
 .popup .popuptext {
 	visibility: hidden;
-	width: 400px;
+	width: 180px;
 	background-color: #555;
 	color: #fff;
-	text-align: left;
+	text-align: center;
 	border-radius: 6px;
 	padding: 8px;
 	position: absolute;
 	z-index: 2000;
-	bottom: 125%;
-	left: 50%;
+	bottom: 100%;
+	left: 1%;
 }
 .popup .popuptext::after {
 	content: "";
@@ -166,10 +188,15 @@ if (empty($urlPid003)){
 
 <script>
 // When the user clicks on div, open the popup
-function myFunctionPop() {
-    var popup = document.getElementById('myPopup');
-    popup.classList.toggle('show');
+function fireFunctionPop() {
+    var fire_popup = document.getElementById('Popup_fire');
+    fire_popup.classList.toggle('show');
 }
+function fruitFunctionPop() {
+    var fruit_popup = document.getElementById('Popup_fruit');
+    fruit_popup.classList.toggle('show');
+}
+
 </script>
 <div id="main">
 	<div id="header">
@@ -180,7 +207,8 @@ function myFunctionPop() {
 			<!--close welcome-->
 			<div id="menubar">
 				<ul id="menu">
-					<li><a href="http://weibo.com/dawanjiaclc">合作商主页 - 大玩家</a></li>
+					<li><a href="http://weibo.com/dawanjiaclc">合作商主页 - 大玩家金牛万达店</a></li>
+					<li><a href="http://60.205.127.180/vendor/dwj/promotion.php">活动通道</a></li>
 				</ul>
 			</div>
 			<!--close menubar--></div>
@@ -190,64 +218,62 @@ function myFunctionPop() {
 		<div class="slideshow">
 			<ul class="slideshow">
 				<li class="show">
-				<img alt="&quot;VRDreams.com.cn&quot;" height="250" src="http://60.205.127.180/php/vendor/dwj/images/home_1.jpg" width="900" /></li>
+				<img alt="www.VRDreams.com.cn" height="250" src="http://60.205.127.180/php/vendor/dwj/images/vrd_logo.png" width="900" /></li>
 				<li>
-				<img alt="" height="250" src="http://60.205.127.180/php/vendor/dwj/images/home_2.jpg" width="900" /></li>
+				<img alt="weibo.com/dawanjiaclc" height="250" src="http://60.205.127.180/php/vendor/dwj/images/dwj_logo.jpg" width="900" /></li>
 			</ul>
 		</div>
 		<!--close slideshow-->
+		
 		<div class="ourwork" style="height: 290px">
-			<h3>VR 火力全开</h3>
-			<p>微信二维码支付<br />
-			单次游戏:30RMB<br />
-			双人游戏:50RMB</p>
-			<div class="popup" onclick="myFunctionPop()">
-				内容介绍 <span id="myPopup" class="popuptext">《神秘海域》是集解谜、寻宝、探险、射击于一身的冒险动作游戏系列。PS3\PS4系列由SCE旗下Naughty 
-				Dog（顽皮狗）工作室制作，PSV版则由Bend Studio制作，为外传性质作品。第一部作品于2007年发售。游戏设定在原始丛林、热带雨林、沙漠腹地、雪山高原、古代遗迹等地，以电影方式呈现。主人公Nathan 
-				Drake（内森·德雷克）要面对各种各样的敌人，最终找到宝藏。该系列游戏同时拥有小说和电影等衍生作品，小说由Christopher 
-				Golden执笔，2011年10月4日推出。电影版从2008年开始筹备，但进度缓慢，预计于2015年初开始拍摄，2016年上映。
+			<h4>VR 全面开火</h4>
+			<h3>单次游戏: 30人民币</h3>
+			<form method="post" style="vertical-align: middle; text-align: center">
+				微信二维码支付<br />
+				<br />
+				<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid002);?>" /> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/fire_dm.png" width="149" />
+			</form>
+			<p></p>
+			<div class="popup" onclick="fireFunctionPop()" style="height: 16px">
+				<h3>双次游戏: 60人民币 - 请点击这里</h3>
+				<span id="Popup_fire" class="popuptext">
+				<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid004);?>" />
 				</span></div>
-			<p>
-			<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid001);?>" /></p>
 			<!--close more--></div>
 		<!--close ourwork-->
+		
 		<div class="testimonials" style="height: 290px">
-			<h3>VR 疯狂水果</h3>
-			<p>微信二维码支付<br />
-			单次游戏:30RMB<br />
-			双人游戏:50RMB</p>
-			<div class="popup" onclick="myFunctionPop()">
-				内容介绍 <span id="myPopup" class="popuptext">《神秘海域》是集解谜、寻宝、探险、射击于一身的冒险动作游戏系列。PS3\PS4系列由SCE旗下Naughty 
-				Dog（顽皮狗）工作室制作，PSV版则由Bend Studio制作，为外传性质作品。第一部作品于2007年发售。游戏设定在原始丛林、热带雨林、沙漠腹地、雪山高原、古代遗迹等地，以电影方式呈现。主人公Nathan 
-				Drake（内森·德雷克）要面对各种各样的敌人，最终找到宝藏。该系列游戏同时拥有小说和电影等衍生作品，小说由Christopher 
-				Golden执笔，2011年10月4日推出。电影版从2008年开始筹备，但进度缓慢，预计于2015年初开始拍摄，2016年上映。</span>
+			<h4>VR 水果先锋</h4>
+			<h3>单次游戏: 10人民币</h3>
+
+			<form method="post" style="vertical-align: middle; text-align: center">
+				微信二维码支付<br />
+				<br />
+				<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid001);?>" />  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/fruit_dm.png" width="149" />
+			</form>
+			<p></p>
+			<div class="popup" onclick="fruitFunctionPop()" style="height: 16px">
+				<h3>双次游戏: 20人民币 - 请点击这里</h3>
+				<span id="Popup_fruit" class="popuptext"> 
+					<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid003);?>" />
+				</span>
 			</div>
-			<p>
-			<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid002);?>" /></p>
+
 			<!--close more--></div>
 		<!--close testimonials-->
-		<div class="projects" style="height: 290px">
-			<h3>VR 最高危机</h3>
-			<p>微信二维码支付<br />
-			单次游戏:30RMB<br />
-			双人游戏:50RMB</p>
-			<div class="popup" onclick="myFunctionPop()">
-				内容介绍 <span id="myPopup" class="popuptext">《神秘海域》是集解谜、寻宝、探险、射击于一身的冒险动作游戏系列。PS3\PS4系列由SCE旗下Naughty 
-				Dog（顽皮狗）工作室制作，PSV版则由Bend Studio制作，为外传性质作品。第一部作品于2007年发售。游戏设定在原始丛林、热带雨林、沙漠腹地、雪山高原、古代遗迹等地，以电影方式呈现。主人公Nathan 
-				Drake（内森·德雷克）要面对各种各样的敌人，最终找到宝藏。该系列游戏同时拥有小说和电影等衍生作品，小说由Christopher 
-				Golden执笔，2011年10月4日推出。电影版从2008年开始筹备，但进度缓慢，预计于2015年初开始拍摄，2016年上映。</span>
-			</div>
-			<p>
-			<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid003);?>" /></p>
-			<!--close more--></div>
-		<!--close projects-->
+
 		<div id="content">
 			<div class="content_item">
 				<h1>合作伙伴</h1>
 				<p>&nbsp;</p>
-				<form method="post">
-					<input height="122" name="Image_partner001" src="http://60.205.127.180/php/vendor/dwj/images/GSTX_logo_s.png" type="image" width="100" />&nbsp;&nbsp;&nbsp;&nbsp;
-					<input height="104" name="Image_partner002" src="http://60.205.127.180/php/vendor/dwj/images/wx_pay_logo_s.png" type="image" width="100" /></form>
+				<form method="post" style="vertical-align: middle; text-align: center">
+					<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/vive_logo.png" width="100" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/unity_3d.png" width="100" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/unreal_logo.png" width="100" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/wxpay_logo.png" width="100" />
+				</form>
 				<p></p>
 			</div>
 			<!--close content_item--></div>
@@ -264,4 +290,3 @@ function myFunctionPop() {
 </body>
 
 </html>
-
---
+++ PROD/www/php/vendor/dwj/promotion.php
@@ -0,0 +1,119 @@
+<?php
+
+//logic to enable WXpay QRcode
+
+ini_set('date.timezone','Asia/Shanghai');
+
+require_once "/home/www/php/wxengine/lib/WxPay.Api.php";
+require_once "/home/www/php/wxengine/logic/WxPay.NativePay.php";
+require_once "/home/www/php/wxengine/logic/log.php";
+
+$notifyPromo = new NativePay();
+
+$WxPayUnifiedOrderP = new WxPayUnifiedOrder();
+$inputPid001P = &$WxPayUnifiedOrderP;
+$inputPid001P ->SetBody("大玩家金牛万达店_全面开火_DM");
+$inputPid001P ->SetAttach("大玩家金牛万达店");
+$inputPid001P ->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
+$inputPid001P ->SetTotal_fee("2000");
+$inputPid001P ->SetTime_start(date("YmdHis"));
+$inputPid001P ->SetTime_expire(date("YmdHis", time() + 3600));
+$inputPid001P ->SetGoods_tag("全面开火");
+$inputPid001P ->SetNotify_url("http://60.205.127.180/wxengine/logic/notify.php");
+$inputPid001P ->SetTrade_type("NATIVE");
+$inputPid001P ->SetProduct_id("Fire_DWJ_JNWD_DM");
+$resultPid001P = $notifyPromo->GetPayUrl($inputPid001P);
+$urlPid001P = $resultPid001P ["code_url"];
+
+
+// Define your username and password
+$username = "dwj";
+$password = "1024";
+
+if ($_POST['txtUsername'] != $username || $_POST['txtPassword'] != $password) {
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta content="3600" http-equiv="refresh" />
+<meta content="IE=9" http-equiv="X-UA-Compatible" />
+<title>VRDreams - Promotion</title>
+<link href="http://60.205.127.180/php/vendor/dwj/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<h3>请工作人员验证登录</h3>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="form">
+	<label for="txtUsername">
+	<h6>员工工号</h6>
+	</label><input name="txtUsername" title="Enter your Username" type="text" />
+	<label for="txtpassword">
+	<h6>密码</h6>
+	</label>
+	<input name="txtPassword" title="Enter your password" type="password" />
+	<p><input name="Submit" type="submit" value="Login" /></p>
+</form>
+<?php
+
+}
+else {
+
+?>
+<link href="http://60.205.127.180/php/vendor/dwj/css/style.css" rel="stylesheet" type="text/css" />
+<div id="main">
+	<div id="header">
+		<div id="banner">
+			<div id="welcome">
+				<h1>DM促销活动页面！！</h1>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+			</div>
+			<!--close welcome-->
+			<div id="menubar">
+				<ul id="menu">
+					<li><a href="http://60.205.127.180/vendor/dwj/index.php">返回大玩家金牛万达店VR主页</a></li>
+				</ul>
+			</div>
+			<!--close menubar--></div>
+		<!--close banner--></div>
+
+	<div class="ourwork" style="height: 290px">
+			<h4>VR 全面开火</h4>
+			<h3>单次游戏: 30人民币 - 活动减10元</h3>
+			<form method="post" style="vertical-align: middle; text-align: center">
+				微信二维码支付<br />
+				<br />
+				<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid001P);?>" /> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<img alt="Photo landscape" src="http://60.205.127.180/php/vendor/dwj/images/fire_dm.png" width="149" />
+			</form>
+			<!--close more--></div>
+		<!--close ourwork-->
+
+		<div class="testimonials" style="height: 290px">
+			<h4>VR 水果先锋</h4>
+			<h3>单次游戏: 10人民币 - 无活动</h3>
+
+			<!--<form method="post" style="vertical-align: middle; text-align: center">
+				微信二维码支付<br />
+				<br />
+				<img alt="Photo landscape" src="http://60.205.127.180/wxengine/logic/qrcode.php?data=<?php echo urlencode($urlPid001);?>" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<input height="104" name="Image_partner004" src="http://60.205.127.180/php/vendor/dwj/images/wx_pay_logo_s.png" type="image" width="100" />
+			</form>-->
+			<!--close more--></div>
+		<!--close testimonials-->
+
+
+</div>
+<!--close main-->
+
+<?php
+
+}
+
+?>
+
+</body>
+
+</html>
